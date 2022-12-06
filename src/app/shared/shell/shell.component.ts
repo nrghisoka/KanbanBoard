9eb95af429d820fe03ab5da0c5bcdcf858531dff
Connor <connorwhisman17@gmail.com>
2022-12-06 01:17:14 -0500

    Start building sidenav

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-shell',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class ShellComponent {
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 }
